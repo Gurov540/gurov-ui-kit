1cba825900b8c8c3a75810ee77e19a4b3d3309cb
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2026-06-07 20:15:44 +0300

    add sizes

---
+++ src/variables/sizes/index.ts
@@ -0,0 +1,23 @@
+const size = {
+  xxxs: 2,
+  xxs: 4,
+  xs: 8,
+  sm: 12,
+  size: 16,
+  ms: 18,
+  md: 20,
+  lg: 28,
+  xl: 32,
+  xxl: 44,
+};
+const height = {
+  xs: 16,
+  sm: 22,
+  height: 32,
+  lg: 40,
+};
+
+export default {
+  size,
+  height,
+};
