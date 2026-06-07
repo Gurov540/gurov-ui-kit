de32e696c1fc082fc476b85ea436573356001cd6
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2026-06-07 20:13:21 +0300

    add base colors

---
+++ src/variables/colors/base.ts
@@ -0,0 +1,29 @@
+export const base = {
+  white: "#FFFFFF",
+  blue: {
+    1: {
+      light: "#111A2C",
+      dark: "#E6F4FF",
+    },
+    2: {
+      light: "#BAE0FF",
+      dark: "#112545",
+    },
+    3: {
+      light: "#91CAFF",
+      dark: "#15325B",
+    },
+    4: {
+      light: "#69B1FF",
+      dark: "#15417E",
+    },
+    5: {
+      light: "#4096FF",
+      dark: "#1554AD",
+    },
+    6: {
+      light: "#1677FF",
+      dark: "#166BDC",
+    },
+  },
+};
