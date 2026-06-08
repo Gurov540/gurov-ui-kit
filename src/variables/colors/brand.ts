10d97345087c03cb961dbf2c682a36cc20626424
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2026-06-08 20:32:34 +0300

    add new brand colors

--- src/variables/colors/brand.ts
+++ src/variables/colors/brand.ts
@@ -6,5 +6,29 @@ export const brand = {
       light: base.blue[6].light,
       dark: base.blue[6].dark,
     },
+    colorPrimaryHover: {
+      light: base.blue[5].light,
+      dark: base.blue[5].dark,
+    },
+    colorPrimaryActive: {
+      light: base.blue[4].light,
+      dark: base.blue[4].dark,
+    },
+    colorPrimaryBg: {
+      light: base.blue[1].light,
+      dark: base.blue[1].dark,
+    },
+    colorPrimaryBgHover: {
+      light: base.blue[2].light,
+      dark: base.blue[2].dark,
+    },
+    colorPrimaryText: {
+      light: base.white,
+      dark: base.white,
+    },
+    colorPrimaryTextOnLight: {
+      light: base.blue[6].light,
+      dark: base.blue[6].dark,
+    },
   },
 };
